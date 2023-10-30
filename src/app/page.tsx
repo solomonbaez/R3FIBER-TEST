24d93e314aa2e32aa897aac5f320f9c68ccfd94d
solomonbaez <solomon.james.baez@gmail.com>
2023-10-30 11:35:37 -0700

    Adjusting carousel for more than two components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import _debounce from "lodash/debounce"
+import _throttle from "lodash/throttle"
 const App = dynamic(() => import("@/components/app"), { ssr: false });
 
 const components: React.JSX.Element[] = [
@@ -12,12 +13,16 @@ const components: React.JSX.Element[] = [
 
   <div key="about" className="h-screen w-screen flex items-center justify-center bg-black-600">
       <h1 className="text-8xl text-white">Hello, World!</h1>
+  </div>,
+
+  <div key="projects" className="h-screen w-screen flex items-center justify-center bg-black-600">
+      <h1 className="text-8xl text-white">This is a test!</h1>
   </div>
 
 ]
 
 const Home: React.FC = () => {
-  const [visibleComponents, setVisibleComponents] = useState<number[]>([0, 1]); // Start with the first two components.
+  const [visibleComponents, setVisibleComponents] = useState<number[]>([0, 1, 2]); // Start with the first two components.
   const newIndex = useRef<number>(0);
   const debounceScroll = useRef<() => void>()
 
@@ -30,22 +35,24 @@ const Home: React.FC = () => {
 
       // Ensure we always have two components in the visibleComponents array.
       if (newIndex.current !== visibleComponents[0]) {
-        const newVisibleComponents = [newIndex.current, newIndex.current + 1];
+        const newVisibleComponents = [newIndex.current, newIndex.current + 1, newIndex.current + 2];
         setVisibleComponents(newVisibleComponents);
         window.scrollTo(0,0)
       }
     };
 
-    debounceScroll.current = _debounce(handleScroll, 200);
+    debounceScroll.current = _debounce(handleScroll, 100);
     const handleDebounce = debounceScroll.current
-    window.addEventListener('scroll', handleDebounce);
+
+    const handleThrottle = _throttle(handleDebounce, 100)
+    window.addEventListener('scroll', handleThrottle);
     return () => {
-      window.removeEventListener('scroll', handleDebounce);
+      window.removeEventListener('scroll', handleThrottle);
     };
   }, [visibleComponents]);
 
   return (
-    <main>
+    <main className="overflow-hidden">
       <div>
         {visibleComponents.map((index) => (
           <div key={index} className="scroll-smooth snap-none overscroll-none">
