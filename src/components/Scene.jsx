b61c7b3eab135dd2cb06ff149bb6c3e24ead71ef
solomonbaez <solomon.james.baez@gmail.com>
2023-10-29 18:05:44 -0700

    Testing if additional rendering is causing vercel failure

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -54,15 +54,15 @@ function Scene(props) {
           <Lightformer form="circle" intensity={2} rotation-y={-Math.PI / 2} position={[10, 1, 0]} scale={8} />
         </group>
       </Environment>
-      <Text
+      {/* <Text
         scale={[1, 1, 2]}
         color="black" // default
         anchorX="center" // default
         anchorY="middle" // default
       >
         HELLO WORLD
-      </Text>
-      <Stats />
+      </Text> */}
+      {/* <Stats /> */}
     </>
   )
 }
