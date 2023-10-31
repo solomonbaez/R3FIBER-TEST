2c6042b02b15c628f4ddb06175fcf85ebf87a055
solomonbaez <solomon.james.baez@gmail.com>
2023-10-30 22:47:24 -0700

    Added naive locomotive smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,63 +3,33 @@ import React, { useState, useEffect, useRef } from "react";
 import dynamic from "next/dynamic";
 import _debounce from "lodash/debounce"
 import _throttle from "lodash/throttle"
-import smoothscroll from "smoothscroll-polyfill";
+
 const App = dynamic(() => import("@/components/app"), { ssr: false });
 
-const components: React.JSX.Element[] = [
-
-  <div key="dashboard" className="h-screen w-screen flex items-center justify-center">
-    <App/>
-  </div>,
-
-  <div key="about" className="h-screen w-screen flex items-center justify-center bg-black-600">
-      <h1 className="text-8xl text-white">Hello, World!</h1>
-  </div>,
-
-  <div key="projects" className="h-screen w-screen flex items-center justify-center bg-black-600">
-      <h1 className="text-8xl text-white">This is a test!</h1>
-  </div>
-
-]
-
 const Home: React.FC = () => {
-  const [visibleComponents, setVisibleComponents] = useState<number[]>([0, 1, 2]); // Start with the first two components.
-  const newIndex = useRef<number>(0);
-  const debounceScroll = useRef<() => void>()
 
   useEffect(() => {
-
-    smoothscroll.polyfill()
-    const windowHeight = window.innerHeight;
-
-    const handleScroll = () => {
-      newIndex.current += Math.floor(window.scrollY / windowHeight);
-      console.log(newIndex, components.length)
-
-      // Ensure we always have two components in the visibleComponents array.
-      if (newIndex.current !== visibleComponents[0]) {
-        const newVisibleComponents = [newIndex.current, newIndex.current + 1, newIndex.current + 2];
-        setVisibleComponents(newVisibleComponents);
+    (
+      async () => {
+        const LocomotiveScroll = (await import("locomotive-scroll")).default;
+        const locomotiveScroll = new LocomotiveScroll();
       }
-    };
-
-    debounceScroll.current = _debounce(handleScroll, 100);
-    const handleDebounce = debounceScroll.current
-    const handleThrottle = _throttle(handleDebounce, 100)
-    window.addEventListener('scroll', handleThrottle);
-    return () => {
-      window.removeEventListener('scroll', handleThrottle);
-    };
-  }, [visibleComponents]);
+    )()
+  }, []);
 
   return (
     <main className="overflow-hidden">
-      <div>
-        {visibleComponents.map((index) => (
-          <div key={index} className="scroll-smooth snap-none overscroll-none">
-            {components[index % (components.length)]}
-          </div>
-        ))}
+
+      <div key="dashboard" className="h-screen w-screen flex items-center justify-center">
+        <App/>
+      </div>
+
+      <div key="about" className="h-screen w-screen flex items-center justify-center bg-black-600">
+          <h1 className="text-8xl text-white">Hello, World!</h1>
+      </div>
+
+      <div key="projects" className="h-screen w-screen flex items-center justify-center bg-black-600">
+          <h1 className="text-8xl text-white">This is a test!</h1>
       </div>
     </main>
   );
