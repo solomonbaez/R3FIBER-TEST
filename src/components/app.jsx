c86027b4a30c59254546193867fc1e746d277835
solomonbaez <solomon.james.baez@gmail.com>
2023-11-02 16:55:44 -0700

    Experimenting with mutable shapes

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,13 @@
 "use client";
+import React from 'react'
 import dynamic from "next/dynamic";
 // import { Canvas } from "@react-three/offscreen"
-import { Canvas } from "@react-three/fiber"
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { useSpring } from '@react-spring/core'
+import { a } from '@react-spring/web'
+import Scene from '@/components/balance'
+
 
 // const AppScene = dynamic(() => import("./positivity"), {ssr: false})
 // const worker = new Worker(new URL('./worker.jsx', import.meta.url))
@@ -9,6 +15,8 @@ import { Canvas } from "@react-three/fiber"
 const Spaciality = dynamic(() => import("./spaciality"), {ssr: false})
 
 export default function App() {
+  const [{ background, fill }, set] = useSpring({ background: '#f0f0f0', fill: '#202020' }, [])
+
   return (
     // <Canvas
     //     worker={worker} fallback={<AppScene />}
@@ -24,6 +32,12 @@ export default function App() {
     // >
     //     <AppScene />
     // </Canvas>
-    <Spaciality />
+    // <Spaciality />
+    <a.main style={{ background }}>
+      <Canvas className="canvas" dpr={[1, 2]}>
+        <Scene setBg={set} />
+        <OrbitControls enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+      </Canvas>
+    </a.main>
   )
 }
